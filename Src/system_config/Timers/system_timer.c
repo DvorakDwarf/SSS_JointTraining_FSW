439c062f8e442e51f545df6e16fbc135c9eb4ba8
Dilraj Gill <=>
2024-09-10 15:29:17 -0700

    Reimplemented SysTick

--- Src/system_config/Timers/system_timer.c
+++ Src/system_config/Timers/system_timer.c
@@ -33,12 +33,55 @@ uint64_t getSysTime() {
 /*                                 SYSTICK                                   */
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 
+//void systick_init() {
+//	// configure for 1 ms period
+//	SysTick->LOAD = (core_MHz / 8) * 1000;
+//	// use AHB/8 as input clock, and enable counter interrupt
+//	SysTick->CTRL = 0x3;
+//	NVIC_EnableIRQ(SysTick_IRQn);
+//}
+
 void systick_init() {
-	// configure for 1 ms period
-	SysTick->LOAD = (core_MHz / 8) * 1000;
-	// use AHB/8 as input clock, and enable counter interrupt
-	SysTick->CTRL = 0x3;
-	NVIC_EnableIRQ(SysTick_IRQn);
+	// Clock Ticks for 1 SysTick period
+	// (10Mhz) * (1ms) = 10^4 ticks
+	uint32_t clockTicks = 1000000;
+
+	// subtract 1 because SysTick is an (n-1) to 0 counter
+	// That's our reload value
+	SysTick->LOAD = (uint32_t)clockTicks - 1;
+
+	// Clear current SysTick counter value
+	SysTick->VAL = (uint32_t) 0;
+
+
+
+	// Program Control and Status Register
+	// SysTick->CTRL[2]: CLKSOURCE to AHB/8
+	// SysTick->CTRL[1]: enable TICKINT to enable SysTick exception
+	// SysTick->CTRL[0]: enable counter
+	SysTick->CTRL = (0b011);
+}
+
+static int led_num = 0;
+
+void SysTick_Handler() {
+	led_num++;
+	if(led_num > 5) led_num = 0;
+
+	if(led_num > 0) { led_d0(1) }
+	else { led_d0(0); }
+
+	if(led_num > 1) { led_d1(1) }
+	else { led_d1(0); }
+
+	if(led_num > 2) { led_d2(1) }
+	else { led_d2(0); }
+
+	if(led_num > 3) { led_d3(1) }
+	else { led_d3(0); }
+
+	if(led_num > 4) { led_hb(1) }
+	else { led_hb(0); }
 }
 
 
@@ -50,7 +93,7 @@ void systick_init() {
  * @param None
  * @returns None
  */
-void SysTick_Handler() {
-	systick_time++;
-	blinky();
-}
+//void SysTick_Handler() {
+//	systick_time++;
+//	blinky();
+//}
